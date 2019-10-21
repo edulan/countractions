3e04a9d729abc8f5049f9195302cfc5abf622231
Eduardo Lanchares <eduardo@elanchares.com>
2019-10-21 20:19:54 +0200

    Prepare PWA

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   globals: {
+    process: "readonly",
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+serviceWorker.register();

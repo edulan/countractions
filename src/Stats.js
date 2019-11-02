b274f9c1a7f6d33e33fe4aa8d001e4ba772d24b3
Eduardo Lanchares <eduardo@elanchares.com>
2019-11-02 06:30:16 +0100

    Add cycles number

--- src/Stats.js
+++ src/Stats.js
@@ -2,11 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import FormattedTime from "./FormattedTime";
 
-const FREQUENCY_SAMPLE_SIZE = 5;
-
 function calculateFrequency(cycles) {
   const timestamps = cycles
-    .slice(0, FREQUENCY_SAMPLE_SIZE)
+    .slice(0, -1)
     .map(({ interval }) => interval)
     .filter(interval => interval > 0);
 
@@ -48,39 +46,49 @@ function calculateElapsedTime(cycles) {
   return firstTick.date - lastTick.date;
 }
 
-const millisInAnHour = 60 * 60 * 1000;
+const MILLIS_IN_AN_HOUR = 60 * 60 * 1000;
 
 function Stats({ cycles }) {
-  const lastHourCycles = cycles.filter(
-    ({ date }) => date > Date.now() - millisInAnHour
-  );
+  const [lastCycle, ...restCycles] = cycles;
+  const lastHourCycles = lastCycle
+    ? [
+        lastCycle,
+        ...restCycles.filter(
+          ({ date }) => date > lastCycle.date - MILLIS_IN_AN_HOUR
+        )
+      ]
+    : [];
   const frequency = calculateFrequency(lastHourCycles);
   const duration = calculateDuration(lastHourCycles);
   const elapsedTime = calculateElapsedTime(lastHourCycles);
 
   return (
-    <>
-      <div className="StatsContainer">
-        <div className="StatsItem">
-          <span className="MeasureTitle">Frequency</span>
-          <div className="MeasureTime">
-            <FormattedTime value={frequency} />
-          </div>
-        </div>
-        <div className="StatsItem">
-          <span className="MeasureTitle">Duration</span>
-          <div className="MeasureTime">
-            <FormattedTime value={duration} />
-          </div>
-        </div>
-        <div className="StatsItem">
-          <span className="MeasureTitle">Timing</span>
-          <div className="MeasureTime">
-            <FormattedTime value={elapsedTime} />
-          </div>
+    <div className="StatsContainer">
+      <div className="StatsItem">
+        <span className="MeasureTitle">Frequency</span>
+        <div className="MeasureTime">
+          <FormattedTime value={frequency} />
         </div>
       </div>
-    </>
+      <div className="StatsItem">
+        <span className="MeasureTitle">Duration</span>
+        <div className="MeasureTime">
+          <FormattedTime value={duration} />
+        </div>
+      </div>
+      <div className="StatsItem">
+        <span className="MeasureTitle">Timing</span>
+        <div className="MeasureTime">
+          <FormattedTime value={elapsedTime} />
+        </div>
+      </div>
+      <div className="StatsItem">
+        <span className="MeasureTitle">Cycles</span>
+        <div className="MeasureTime">
+          <span className="TimeDigit">{lastHourCycles.length}</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
